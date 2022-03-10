dba151390fef28ed05c8ba69838781a13bff87d0
gywn-dev <gyuwonlee.olivia@gmail.com>
2022-03-10 12:27:00 +0900

    블로그 배포

--- public/sw.js
+++ public/sw.js
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "framework-0e1bf37de02f7cef2174.js"
   },
   {
-    "url": "app-b4b95fee99f9e0e6d65d.js"
+    "url": "app-0cfefa033cf511e62407.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "383da1eeb2e17a7572ff36789bd9c069"
+    "revision": "38510a55a73f1cd5e11b056279016223"
   },
   {
     "url": "static/webfonts/s/roboto/v29/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -59,7 +59,7 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "4d23ecd33747f0d266d718dbd156c745"
+    "revision": "6a5f2b39950954e7e4bb0b27688dc8d1"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -151,7 +151,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-b4b95fee99f9e0e6d65d.js`))) {
+  if (!resources || !(await caches.match(`/app-0cfefa033cf511e62407.js`))) {
     return await fetch(event.request)
   }
 
